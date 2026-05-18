2f21097db04a141eb38bf6a44791c317e6a9c01f
Logic777Killer <ilyakopiy@gmail.com>
2026-05-18 20:46:52 +0300

    правка для деплоя

--- internal/database/db.go
+++ internal/database/db.go
@@ -14,14 +14,14 @@ var DB *sql.DB
 func InitDB(cfg *config.Config) error {
 	var err error
 
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
 		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
 
 	DB, err = sql.Open("postgres", connStr)
 	if err != nil {
 		return fmt.Errorf("error opening db: %w", err)
 	}
-	
+
 	err = DB.Ping()
 	if err != nil {
 		return fmt.Errorf("error connecting to db: %w", err)
